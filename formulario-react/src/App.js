e460e3283b250486486816a8e63e3d4f758bd16a
Henrique <henrique.dsvlima@gmail.com>
2021-09-22 14:16:53 -0300

    Commit inicial - estruturando o projeto

---
+++ formulario-react/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import FormularioCadastro from './Components/FormularioCadastro/FormularioCadastro';
+
+function App() {
+  return (
+    <>
+    <h1> Formulário de Cadastro</h1>
+    <FormularioCadastro />
+    </>
+  )
+}
+
+export default App;
---
+++ formulario-react/src/Components/FormularioCadastro/FormularioCadastro.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function FormularioCadastro() {
+    return(
+    <form >
+        <input type="text"></input>
+    </form>
+    )
+}
+export default FormularioCadastro;
