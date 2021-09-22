d5c794ba5d7265de8cee81498c42af4de6676dc7
Henrique <henrique.dsvlima@gmail.com>
2021-09-22 14:33:17 -0300

    inserido material-ui

--- formulario-react/src/Components/FormularioCadastro/FormularioCadastro.jsx
+++ formulario-react/src/Components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import Button from '@material-ui/core/Button'
 
 function FormularioCadastro() {
     return(
-    <form >
+    <form > 
+        <label> Nome </label>
         <input type="text"></input>
+
+        <label> Sobrenome </label>
+        <input type="text"></input>
+
+        <label> CPF </label>
+        <input type="text"></input>
+
+
+        <label> Promoções </label>
+        <input type="checkbox"></input>
+
+        <label> Novidades</label>
+        <input type="checkbox" />
+
+        <Button variant='contained' color="primary"> 
+            Cadastrar
+         </Button>
+
     </form>
     )
 }
